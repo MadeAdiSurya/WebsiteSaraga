8491aea41f7ce945ba6a28baf799d7a576b8f393
Made Adi Surya Pramana <18220105@std.stei.itb.ac.id>
2023-04-11 20:57:11 +0700

    create header.js and footer.js

---
+++ components/header.js
@@ -0,0 +1,27 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `<header>
+            <div class="wrapper">
+              <div class="nav">
+                <div class="logo-saraga">
+                  <img src="./img/logo-saraga.png" alt="logo-saraga" />
+                </div>
+                <div class="menu-navbar">
+                  <ul>
+                    <li><a class="btn pesan" href="#">Pesan</a></li>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Bantuan</a></li>
+                    <li><a class="profile-icon" href="#"></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </header>`;
+  }
+}
+
+customElements.define("header-elements", Header);
